dfc802b93f0a65333490f5b2eebdc3543ba1f88f
Gianni Schiralli <gschiralli@myseneca.ca>
2022-01-24 16:13:41 -0500

    server setup

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-const { Console } = require("console");
 const express = require("express");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.json({ message: "Up and running" });
-});
 
 app.listen(PORT, () => {
   console.log(`Rest-Inn API is up and running on PORT ${PORT}`);
