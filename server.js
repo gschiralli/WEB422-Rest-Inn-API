3536534f13dfd0b40b6ed22409701ce27287e375
Gianni Schiralli <gschiralli@myseneca.ca>
2022-01-27 00:09:44 -0500

    Mongodb connection setup

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config({ path: "config/keys.env" });
 const customerController = require("./controllers/Customer.js");
 
@@ -11,6 +12,13 @@ app.use("/customers", customerController);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Rest-Inn API is up and running on PORT ${PORT}`);
+
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+    console.log(`Connected to MongoDB`);
+  } catch (error) {
+    console.log(error);
+  }
 });
