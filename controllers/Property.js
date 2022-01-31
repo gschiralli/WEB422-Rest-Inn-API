7be00fcdd5ecffe29934cd98575c43d1c370a9eb
Gianni Schiralli <gschiralli@myseneca.ca>
2022-01-31 16:28:40 -0500

    Update and Get property routes completed

--- controllers/Property.js
+++ controllers/Property.js
@@ -11,4 +11,13 @@ router.get("/", propertyService.getAllProperties);
 //Retrieve all property types
 router.get("/types", propertyService.getAllPropertyTypes);
 
+//Retrieve all best sellers
+router.get("/best-sellers", propertyService.getBestSellers);
+
+//Retrieve property by id
+router.get("/:id", propertyService.getPropertyByID);
+
+//Update property by id
+router.put("/:id", propertyService.updatePropertyByID);
+
 module.exports = router;
--- services/Property.js
+++ services/Property.js
@@ -12,16 +12,85 @@ exports.createProperty = async (req, res) => {
 };
 
 exports.getAllProperties = async (req, res) => {
+  const queryString = {};
+  let message = "List of all the properties";
+
+  if (req.query.location) {
+    queryString.location = req.query.location;
+    message = `List of all the properties that are located in ${queryString.location}`;
+  } else if (req.query.type) {
+    queryString.type = req.query.type;
+    message = `List of all the properties of type ${queryString.type}`;
+  }
+
   try {
-    const properties = await propertyModel.find();
+    const properties = await propertyModel.find(queryString);
+
     res.json({
-      message: "list of all the properties in the database",
+      message,
       data: properties,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-  res.json({ message: "Get all properties working" });
 };
 
-exports.getAllPropertyTypes = async (req, res) => {};
+exports.getAllPropertyTypes = async (req, res) => {
+  try {
+    const propertyTypes = await propertyModel.find().distinct("type");
+
+    res.json({
+      message: "List of all the property types",
+      data: propertyTypes,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
+
+exports.getBestSellers = async (req, res) => {
+  try {
+    const bestSellers = await propertyModel.find({ bestSeller: true });
+
+    res.json({
+      message: "List of all the properties that are best sellers",
+      data: bestSellers,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getPropertyByID = async (req, res) => {
+  try {
+    const property = await propertyModel.findById(req.params.id);
+    if (property) {
+      res.json({
+        message: `Property with the id of ${req.params.id}`,
+        data: property,
+      });
+    } else {
+      res.json({
+        message: `There is no property with the id of ${req.params.id} in the database`,
+      });
+    }
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+exports.updatePropertyByID = async (req, res) => {
+  try {
+    const property = await propertyModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.json({ message: "Update successful", data: property });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
